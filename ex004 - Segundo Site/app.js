a24193bf56fd5919d2f6f744cb74f29756e457d6
Tony <tonytnportela@outlook.com>
2022-10-06 18:18:07 -0300

    Segundo Site e atualizações do Primeiro

---
+++ ex004 - Segundo Site/app.js
@@ -0,0 +1,56 @@
+function verificar() {
+  let data = new Date();
+  let ano = data.getFullYear();
+  let fano = document.getElementById('ano');
+  let res = document.getElementById('res');
+
+  if (fano.value == 0 || fano.value > ano) {
+    window.alert('Verifique os dados e tente novamente.');
+  } else {
+    let fsex = document.getElementsByName('radsex');
+    let idade = ano - Number(fano.value);
+    let gênero = ''
+    // res innerhtml
+    let img = document.createElement('img');
+    
+
+    if (fsex[0].checked) {
+      gênero = 'homem'
+      if (idade > 0 && idade <= 13) {
+        //criança
+        img.setAttribute('src', 'foto-bebe-m.png');
+      } else if (idade > 13 && idade < 18) {
+        //adolescente
+        img.setAttribute('src', 'foto-jovem-m.png');
+      } else if (idade > 18 && idade < 65) {
+        //adulto
+        img.setAttribute('src', 'foto-adulto-m.png');
+      } else {
+        //idoso
+        img.setAttribute('src', 'foto-idoso-m.png');
+      }
+    } else if (fsex[1].checked) {
+      gênero = 'mulher'
+      if (idade > 0 && idade <= 13) {
+        //criança
+        img.setAttribute('src', 'foto-bebe-f.png');
+      } else if (idade > 13 && idade < 18) {
+        //adolescente
+        img.setAttribute('src', 'foto-jovem-f.png');
+      } else if (idade > 18 && idade < 65) {
+        //adulto
+        img.setAttribute('src', 'foto-adulto-f.png');
+      } else {
+        //idoso
+        img.setAttribute('src', 'foto-idoso-f.png');
+      }
+    }
+
+    res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+    res.appendChild(img);
+
+    
+
+    
+  }
+}
