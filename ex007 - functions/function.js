df73d5470caad0e62e4fbe14661f46579c308c63
Tony <tonytnportela@outlook.com>
2022-10-18 19:47:19 -0300

    funções !

---
+++ ex007 - functions/function.js
@@ -0,0 +1,25 @@
+function parimp(n) {
+  if (n % 2 == 0) {
+    return 'par';
+  } else {
+    return 'impar';
+  }
+}
+
+console.log(parimp(9));
+
+// -------------------------------------------------------------------- //
+
+function soma(n1, n2) {
+  return n1 + n2;
+}
+
+console.log(soma(432894234, 32490324))
+
+// -------------------------------------------------------------------- //
+
+const res = (x) => {
+  return x*2
+}
+
+console.log(res(10))
